7015a17b179e4b0ee18e8fa774282c6535a13679
VladAlpin <zxczxczxc33513@gmail.com>
2023-05-24 23:17:49 +1000

    pp114

---
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -0,0 +1,28 @@
+package jm.task.core.jdbc.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Util {
+
+    private static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String URL = "jdbc:mysql://localhost:3306/test?autoReconnect=true&useSSL=false";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName(DB_DRIVER);
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            System.out.println("good connection!");
+        } catch (SQLException e) {
+            System.err.println("no good connection!" + e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return connection;
+    }
+
+}
